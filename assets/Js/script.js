d68763e12c55caab9d5a703df7771a7e47eec782
Alphacenturai001 <edooalaam@gmail.com>
2022-07-28 19:44:08 -0400

    add all js

--- assets/Js/script.js
+++ assets/Js/script.js
@@ -1,1 +1,16 @@
-console.log("hello world")
+// Initialize and add the map
+function initMap() {
+    // The location of Uluru
+    const Florida = { lat: 28.5383, lng: -81.379234, };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 8,
+      center: Florida,
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+      position: Florida,
+      map: map,
+    });
+  }
+  
